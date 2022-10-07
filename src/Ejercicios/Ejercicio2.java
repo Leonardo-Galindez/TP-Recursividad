f7463f3c1b3e55fa61448a107f51e9752b0a830f
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-07 18:21:39 -0300

    cambios del ejercicios

--- src/Ejercicios/Ejercicio2.java
+++ src/Ejercicios/Ejercicio2.java
@@ -11,5 +11,6 @@ package Ejercicios;
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("como estas");
     }
 }
