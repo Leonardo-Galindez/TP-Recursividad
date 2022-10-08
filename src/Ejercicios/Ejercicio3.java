2ed21ebe6061c995ff32feb3f64aa46037e3b22f
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-08 02:04:58 -0300

    ejercicio 4 termindo

--- src/Ejercicios/Ejercicio3.java
+++ src/Ejercicios/Ejercicio3.java
@@ -9,7 +9,19 @@ package Ejercicios;
  * @author galin
  */
 public class Ejercicio3 {
-    public static void main(String[] args) {
-        System.out.println("");
+
+    public static void main(String[] args) { //averiguar que hace mistery
+        String numero;
+        numero = "123456789";
+        int j;
+        j = numero.length() - 1;
+        mistery(j, numero);
+    }
+
+    public static void mistery(int p, String a) {
+        if (p > 0) {
+            System.out.println(a.charAt(p));
+            mistery(p - 1, a);
+        }
     }
 }
--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -8,8 +8,33 @@ package Ejercicios;
  *
  * @author galin
  */
+
+
 public class Ejercicio4 {
+
     public static void main(String[] args) {
-        System.out.println("");
+        int num = 1111111, resto;
+        resto = num % 10;
+        boolean valorNum = true;
+        valorNum = VerificarNum(resto, num, valorNum);
+        System.out.println(valorNum);
+
+    }
+
+    public static boolean VerificarNum(int resto, int num, boolean valorNum) {
+
+        int res;
+        res = num % 10;
+        if(valorNum && num>0){
+            if (res == resto) {
+                valorNum = true;
+            } else {
+                valorNum = false;
+            }
+            valorNum = VerificarNum(resto, num / 10, valorNum);
+        }
+        
+
+        return valorNum;
     }
 }
