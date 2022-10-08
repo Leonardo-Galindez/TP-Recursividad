0e45773884c751cfbfc252d74aee5425d9578667
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-08 18:43:28 -0300

    Ejercicio 5 terminado

---
+++ src/Ejercicios/Ejercicio5.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author galin
+ */
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        int cont;
+        cont=ContarMult3();
+        System.out.println(cont);
+
+    }
+
+    public static int ContarMult3() {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int cont = 0;
+        System.out.println("Ingrese numero");
+        num = sc.nextInt();
+        if (num != 0) {
+            if (num % 3 == 0) {
+                cont = 1 + ContarMult3();
+            } else {
+                cont = 0 + ContarMult3();
+            }
+
+        }
+        return cont;
+    }
+
+}
