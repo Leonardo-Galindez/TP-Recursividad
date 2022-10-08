4b8205a1c3ede4b4f0eaaa91fc8ccfd6512cf1a2
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-08 18:04:00 -0300

    documentacion del ejercicio 4

--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -8,8 +8,6 @@ package Ejercicios;
  *
  * @author galin
  */
-
-
 public class Ejercicio4 {
 
     public static void main(String[] args) {
@@ -18,22 +16,22 @@ public class Ejercicio4 {
         boolean valorNum = true;
         valorNum = VerificarNum(resto, num, valorNum);
         System.out.println(valorNum);
-
     }
 
+    //metemos valorNum por parametro, tengo errores para trabajarlo en metodo
+    //resto lo meto por parametros para trabajar a partir de ese numero
     public static boolean VerificarNum(int resto, int num, boolean valorNum) {
 
         int res;
-        res = num % 10;
-        if(valorNum && num>0){
+        res = num % 10;//nuevo resto a comparar con el resto que entra por parametros
+        if (valorNum && num > 0) {//condicion de corte que el numero sea igual al resto que entro por parametros y que num /10 sea mayor a 0 
             if (res == resto) {
                 valorNum = true;
             } else {
                 valorNum = false;
             }
-            valorNum = VerificarNum(resto, num / 10, valorNum);
+            valorNum = VerificarNum(resto, num / 10, valorNum);//varia num cada ves que se llama al metodo se divide por 10
         }
-        
 
         return valorNum;
     }
