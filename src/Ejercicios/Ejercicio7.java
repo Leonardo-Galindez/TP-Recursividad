88cce19fc2c5389c2b296910e77a728a6038eeb0
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-09 00:40:01 -0300

    Plantenado ejercicio 7

---
+++ src/Ejercicios/Ejercicio7.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author galin
+ */
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        
+        int [][]Numeros ={{1,2,3},
+                         {4,5,6},
+                         {7,8,9}};
+        
+    }
+    
+    public static int SumaFilas(int fila,int colum,int [][]Matriz){
+        int suma;
+        suma=0;
+        
+        if(fila<Matriz.length){
+            if(colum<Matriz[0].length){
+                suma=Matriz[fila][colum]+SumaFilas(fila,colum,Matriz);
+            }
+            
+        }
+        return suma;
+    }
+}
