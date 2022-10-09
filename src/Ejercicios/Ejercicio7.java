e077546399e7e90944b907d07acfd92d60681833
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-09 17:36:03 -0300

    cambios en el ejercicio 7

--- src/Ejercicios/Ejercicio7.java
+++ src/Ejercicios/Ejercicio7.java
@@ -9,24 +9,35 @@ package Ejercicios;
  * @author galin
  */
 public class Ejercicio7 {
+
     public static void main(String[] args) {
-        
-        int [][]Numeros ={{1,2,3},
-                         {4,5,6},
-                         {7,8,9}};
-        
+        int suma, fila, colum;
+        fila = 0;
+        colum = 0;
+        int[][] Numeros = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        SumaFilas(fila, colum, Numeros);
     }
-    
-    public static int SumaFilas(int fila,int colum,int [][]Matriz){
+
+    public static int SumaFilas(int fila, int colum, int[][] Matriz) {
         int suma;
-        suma=0;
-        
-        if(fila<Matriz.length){
-            if(colum<Matriz[0].length){
-                suma=Matriz[fila][colum]+SumaFilas(fila,colum+1,Matriz);
+        suma = 0;
+        if (fila!= Matriz.length-1 || colum != Matriz[0].length-1) {
+
+            if (colum < Matriz[0].length) {
+                suma = Matriz[fila][colum] + SumaFilas(fila, colum + 1, Matriz);
+            }else{
+                colum=0;
+               
+                SumaFilas(fila+1,colum,Matriz);
             }
-            
+           
         }
+         
         return suma;
     }
+
 }
