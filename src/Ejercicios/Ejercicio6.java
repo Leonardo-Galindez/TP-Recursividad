9cbe0663f7d97786024432ec13e9e59fddf32b1b
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-09 00:03:04 -0300

    Ejercicio 6 terminado

---
+++ src/Ejercicios/Ejercicio6.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author galin
+ */
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        int i,mayor,posMayor;
+        mayor=0;
+        posMayor=0;
+        i=0;
+        int Numeros[] = {1, 2, 3, 5, 7};
+        System.out.println(Numeros[NumeroMayor(Numeros,i,mayor,posMayor)]);
+
+    }
+
+    public static int NumeroMayor(int[] Array,int i,int mayor,int posMayor) {//siempre entra todo por parametrod??
+        
+        if (i < Array.length) {
+            if (mayor < Array[i]) {
+                mayor = Array[i];
+                posMayor = i;
+            }
+            posMayor=NumeroMayor(Array,i+1,mayor,posMayor);
+        }
+        return posMayor;
+    }
+}
