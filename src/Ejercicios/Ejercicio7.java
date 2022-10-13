395c5f3f96ad1023ec9f82833982784a60b96ed4
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-13 10:59:36 -0300

    ejercicio 7 terminado

--- src/Ejercicios/Ejercicio7.java
+++ src/Ejercicios/Ejercicio7.java
@@ -19,47 +19,52 @@ public class Ejercicio7 {
             {4, 5, 6},
             {7, 8, 9}
         };
+        int []Fila= new int [Numeros[0].length];
         System.out.println("");
-        System.out.println(SumaMatriz(fila, colum, Numeros));
+        System.out.println(SumaMatriz(fila, colum, Numeros,Fila));
     }
 
     public static int SumaFila(int c,int [] Array){
         int suma;
         suma=0;
-        if(c<Array.length){
-            System.out.println(Array[c]);
-            suma=SumaFila(c+1,Array);
-            //suma=Array[c]+SumaFila(c+1,Array);
+        if(c>=0){
+            
+            suma=Array[c]+SumaFila(c-1,Array);
+        }else{
+            
         }
         return suma;
     }
 //otro modulo suma de columas
     //Algoritmo recursivo
-    public static int SumaMatriz(int fila, int colum, int[][] Matriz) {
+    public static int SumaMatriz(int fila, int colum, int[][] Matriz,int []Array) {
         int sumaM = 0,sumaF=0;
-        int []Fila= new int [Matriz[0].length];
+        
         //System.out.print(Matriz[fila][colum] + " ");
         
         if (fila != Matriz.length - 1 || colum != Matriz[0].length - 1) {
             //2
             if (colum <= Matriz[0].length - 1) {
                 System.out.print(Matriz[fila][colum]+" ");
-                Fila[colum]=Matriz[fila][colum];
-                sumaM = Matriz[fila][colum] + SumaMatriz(fila, colum + 1, Matriz);
+                Array[colum]=Matriz[fila][colum];    
+                sumaM = Matriz[fila][colum] + SumaMatriz(fila, colum + 1, Matriz,Array);
 
             } else {
-                System.out.println(SumaFila(colum-1,Fila));
+                System.out.println(SumaFila(colum-1,Array));
                 System.out.println("");
                 colum = 0;
                 fila++;
-                sumaM = SumaMatriz(fila, colum, Matriz);
+                sumaM = SumaMatriz(fila, colum, Matriz,Array);
 
             }
 
         } else {
+            Array[colum]=Matriz[fila][colum]; 
             System.out.print(Matriz[fila][colum]+" ");
             sumaM = Matriz[fila][colum];//caso base
             System.out.println("");
+            System.out.println(SumaFila(colum,Array));
+            
         }
 
         return sumaM;
