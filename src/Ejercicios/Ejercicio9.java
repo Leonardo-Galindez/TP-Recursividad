d3397cece7fcdaa3acae1a431f1b05d7776ce6b8
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-13 11:34:36 -0300

    ejercicio 9 terminado

--- src/Ejercicios/Ejercicio9.java
+++ src/Ejercicios/Ejercicio9.java
@@ -8,8 +8,33 @@ package Ejercicios;
  *
  * @author galin
  */
+import java.util.Scanner;
+
 public class Ejercicio9 {
+
     public static void main(String[] args) {
+        int divisor, dividendo, cociente, resto;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Ingrese Divisor");
+            divisor = sc.nextInt();
+            System.out.println("Ingrese Dividendo");
+            dividendo = sc.nextInt();
+
+            cociente = dividendo / divisor;
+            resto = dividendo % divisor;
+            DivisionResta(dividendo, divisor,cociente,resto);
+        } while (dividendo != 0);
+    }
+
+    public static int DivisionResta(int num1, int num2,int cos, int rest) {
         
+        if (num1!=rest) {
+            System.out.println(DivisionResta(num1-cos,num2,cos,rest));
+        }
+        
+        return num1;
     }
 }
