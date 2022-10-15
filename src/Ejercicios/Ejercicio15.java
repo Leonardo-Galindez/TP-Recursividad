54df97e86fe3154f995df4b3eab4e0a45fb6da23
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-15 02:41:35 -0300

    Ejercicio 15 terminado

--- src/Ejercicios/Ejercicio15.java
+++ src/Ejercicios/Ejercicio15.java
@@ -8,8 +8,31 @@ package Ejercicios;
  *
  * @author galin
  */
+import java.util.Scanner;
 public class Ejercicio15 {
     public static void main(String[] args) {
-        
+        String cadena;
+        int longitud,i,cont;
+        i=0;
+        cont=0;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Ingrese cadena");
+        cadena=sc.nextLine();
+        longitud=cadena.length();
+        cont=ContarVocales(cadena,longitud,i);
+        System.out.println(cont);
+    }
+    
+    public static int ContarVocales(String texto,int lon,int i){
+        int cont;
+        cont=0;
+        if(i<lon){
+            if(texto.charAt(i)=='a' ||texto.charAt(i)=='e' ||texto.charAt(i)=='i' ||texto.charAt(i)=='o' ||texto.charAt(i)=='u'){
+                cont=1+ContarVocales(texto,lon,i+1);
+            }else{
+                cont=ContarVocales(texto,lon,i+1);
+            }
+        }
+        return cont;
     }
 }
