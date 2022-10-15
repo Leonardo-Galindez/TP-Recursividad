d27e08a6921bd6952352abe9a0e4ebf04087fc75
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-15 16:18:54 -0300

    Ejercicio 16 terminado

--- src/Ejercicios/Ejercicio16.java
+++ src/Ejercicios/Ejercicio16.java
@@ -2,21 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Ejercicios.Ejercicio16;
+package Ejercicios;
 
 /**
  *
  * @author galin
  */
+import Ejercicios.POO.Producto;
 import java.util.Scanner;
 
 public class Ejercicio16 {
 
     public static void main(String[] args) {
-
+        double precioBusca;
+        int i=0;
         Scanner sc = new Scanner(System.in);
         Producto[] MisProductos = new Producto[5];
         CargarProducto(MisProductos);
+        System.out.println("Ingrese precio a buscar");
+        precioBusca=sc.nextDouble();
+        System.out.println(ContProducto(MisProductos,precioBusca,i));
     }
 
     public static void CargarProducto(Producto[] Array) {
@@ -27,10 +32,9 @@ public class Ejercicio16 {
         for (int i = 0; i < Array.length; i++) {
             System.out.println("Ingrese codigo");
             cod = sc.nextInt();
-            sc.next();
+            sc.nextLine();
             System.out.println("Ingrese descripcion");
             desc = sc.nextLine();
-            sc.next();
             System.out.println("Ingrese precio por kilo");
             precio = sc.nextDouble();
             System.out.println("Ingrese stock");
@@ -38,11 +42,20 @@ public class Ejercicio16 {
             
             Producto miProducto= new Producto(cod,desc,precio,stock);
             Array[i]=miProducto;
-           
         }
+        
     }
     //cantidad de productos con precio dado
-    public static void ContProducto(){
-        
+    public static int ContProducto(Producto []Productos,double precioBusca,int i){
+        int cont;
+        cont=0;
+        if(i<Productos.length){
+            if(Productos[i].getPrecioKg() == precioBusca){
+                cont=1+ContProducto(Productos,precioBusca,i+1);
+            }else{
+                cont=ContProducto(Productos,precioBusca,i+1);
+            }
+        }
+        return cont;
     }
 }
