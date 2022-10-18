55d7af1faa8b098723f7a888c76e314b7de90b09
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-17 22:35:40 -0300

    Ejercicio 19 cambios

--- src/Ejercicios/Ejercicio19.java
+++ src/Ejercicios/Ejercicio19.java
@@ -14,13 +14,44 @@ public class Ejercicio19 {
 
     public static void main(String[] args) {
 
-        int i, j, renglones;
+        int i, j, altura;
+        int aux;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese renglones");
-        renglones = sc.nextInt();
-        i = 1;
-        j = 1;
-        MediaPiramide(renglones, i);
+        System.out.println("Ingrese altura de la piramide");
+        altura = sc.nextInt();
+        aux = altura;
+        i=1;
+    
+        MediaPiramide(altura, i);
+        //PiramideInvert(i,j,renglones,aux);
+   
+}
+
+public static void Piramide(int altura, int j, int i,int aux) {
+
+        
+        
+        
+        if(i<=altura){
+            if(j<aux){
+                System.out.print(" ");
+                Piramide(altura,j+1,i,aux);
+            }else{
+                System.out.println(i);
+                Piramide(altura,1,i+1,aux-1);
+            }
+        }
+        /*if (i<altura) {
+            if (i <= j) {
+                System.out.print(i);
+                Piramide(altura, j, i + 1);
+            } else {
+                System.out.println("");
+                Piramide(altura,j+1,1);
+
+            }
+        }*/
     }
 
     public static void MediaPiramide(int renglones, int i) {
@@ -28,19 +59,11 @@ public class Ejercicio19 {
         if (i <= renglones) {
 
             MostrarPiramide(i);
-            PiramideInvert(i,renglones);
+
             System.out.println("");
             MediaPiramide(renglones, i + 1);
         }
     }
-    public static void PiramideInvert(int i,int renglones){
-        System.out.println("");
-        if(i<renglones){
-            System.out.println(i);
-            PiramideInvert(i+1,renglones);
-        }
-    }
-
     public static void MostrarPiramide(int i) {
 
         if (i > 0) {
@@ -48,5 +71,33 @@ public class Ejercicio19 {
             MostrarPiramide(i - 1);
         }
     }
+    
+    
 
+    public static void PiramideInvert(int i, int j, int renglones, int aux) {
+
+        if (i < renglones) {
+            if (j < aux) {
+                System.out.println(" ");
+                PiramideInvert(i, j + 1, renglones, aux);
+            } else {
+                aux--;
+                System.out.println(i);
+                PiramideInvert(i + 1, 1, renglones, aux);
+            }
+        } else {
+            System.out.println(i);
+        }
+
+    }
+    
+    public static void MostrarPiramide(int i, int renglones) {
+
+        if (i < renglones) {
+            System.out.print(i);
+            MostrarPiramide(i + 1, renglones);
+        }
+    }
+
+     
 }
