8cf242c3a59586c7b545bd3d9168fafc5d138c63
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-11-08 10:27:12 -0300

    cambios

--- src/EjerciciosExtra/Ejercicio2C.java
+++ src/EjerciciosExtra/Ejercicio2C.java
@@ -22,24 +22,24 @@ public class Ejercicio2C {
         orden = sc.nextInt();
         int Matriz[][] = new int[orden][orden];
         CargarMatriz(Matriz, orden, i, j);
-        System.out.println(SumarDiagonal(Matriz, orden, i, j, sumaDiagonal));
+        System.out.println(SumarDiagonal(Matriz, orden, i, j));
 
     }
 
-    public static int SumarDiagonal(int Matriz[][], int orden, int fila, int colum, int sumaDiagonal) {
-
+    public static int SumarDiagonal(int Matriz[][], int orden, int fila, int colum) {
+        int sumaDiagonal = 0;
         if (fila != orden - 1 || colum != orden - 1) {
             if (colum < orden) {
                 if (fila == colum) {
-                    sumaDiagonal = Matriz[fila][colum] + SumarDiagonal(Matriz, orden, fila, colum + 1, sumaDiagonal);
+                    sumaDiagonal = sumaDiagonal + Matriz[fila][colum] + SumarDiagonal(Matriz, orden, fila, colum + 1);
                 } else {
-                    sumaDiagonal =SumarDiagonal(Matriz, orden, fila, colum + 1, sumaDiagonal);
+                    sumaDiagonal = sumaDiagonal + SumarDiagonal(Matriz, orden, fila, colum + 1);
                 }
             } else {
-                sumaDiagonal =SumarDiagonal(Matriz, orden, fila + 1, 0, sumaDiagonal);
+                sumaDiagonal = SumarDiagonal(Matriz, orden, fila + 1, 0);
             }
-        }else{
-            sumaDiagonal =sumaDiagonal+ Matriz[fila][colum];
+        } else {
+            sumaDiagonal = sumaDiagonal + Matriz[fila][colum];
         }
         return sumaDiagonal;
     }
