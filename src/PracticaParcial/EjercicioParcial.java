a7a16123055a905647f47c5d7632e3a4a85d2659
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-11-23 19:52:11 -0300

    cambios

--- src/PracticaParcial/EjercicioParcial.java
+++ src/PracticaParcial/EjercicioParcial.java
@@ -9,19 +9,51 @@ package PracticaParcial;
  * @author galin
  */
 public class EjercicioParcial {
+
     public static void main(String[] args) {
-        
-        System.out.println(sumaDigitos(3862));
-        
+
+        piramide(4,4,1,1);
+
     }
-    public static int sumaDigitos(int num){
-        int suma=0;
-        
-        if(num>9){
-            suma = suma + num%10+sumaDigitos(num/10);
-        }else{
-            suma=suma+num;
+
+    public static int sumaDigitos(int num) {
+        int suma = 0;
+
+        if (num > 9) {
+            suma = suma + num % 10 + sumaDigitos(num / 10);
+        } else {
+            suma = suma + num;
         }
         return suma;
+    }
+
+    public static void piramide(int altura, int espacios, int posF, int posC) {
+        if (posF <= altura) {
+            if (posC < espacios) {
+                System.out.print(" ");
+                piramide(altura, espacios, posF, posC+1);
+            } else {
+                ladoIzquierdo(posF, 1);
+                System.out.println("");
+                piramide(altura,espacios-1,posF+1,1);
+            }
+        }
+
+    }
+
+    public static void ladoIzquierdo(int cant, int i) {
+        if (cant > i) {
+            System.out.print("*");
+            ladoIzquierdo(cant, i+1);
+        }else{
+            ladoDerecho(cant);
+        }
+    }
+    
+    public static void ladoDerecho(int tope){
+        if(tope>0){
+            System.out.print("*");
+            ladoDerecho(tope-1);
+        }
     }
 }
